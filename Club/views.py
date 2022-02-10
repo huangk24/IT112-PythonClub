11a77144118dc91f6bbd3aafef87aa083e34eff6
huangk24 <huangk24@uw.edu>
2022-02-09 20:56:04 -0800

    assignment 5

--- Club/views.py
+++ Club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
 def index(request):
     return render(request, 'Club/index.html')
+
+def meeting(request):
+    meeting_list = Meeting.objects.all()
+    return render(request, 'Club/meeting.html', {'meeting_list': meeting_list})
