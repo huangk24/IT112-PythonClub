6604bb5c44d7b3102385ff4276972e3df61215da
huangk24 <huangk24@uw.edu>
2022-03-01 15:39:56 -0800

    a7 test

--- Club/tests.py
+++ Club/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Meeting, MeetingMinutes, Resource, Event
+import datetime
 
 # Create your tests here.
+class MeetingTest(TestCase):
+    def setUp(self):
+        self.type = Meeting(meetingtitle = 'New Year')
+
+    def test_typestring(self):
+        self.assertEqual(str(self.type), 'New Year')
+
+    def test_tablename(self):
+        self.assertEqual(str(Meeting._meta.db_table), 'meeting')
+
+class EventTest(TestCase):
+    def setUp(self):
+        self.type = Meeting(meetingtitle = 'New Year')
+        self.user = User(username = 'user1')
+        self.event = Event(eventtitle = 'New Year', location = 'Seattle', 
+                           date = datetime.date(2022, 3, 1), description = 'Celebrate Chinese New Year')
+
+    def test_string(self):
+        self.assertEqual(str(self.event), 'New Year')
+
+    def test_location(self):
+        self.assertEqual(str(self.event.location), 'Seattle')
+
+class ResourceTest(TestCase):
+    def setUp(self):
+        self.type = Resource(resoucename = 'Dumplings')
+        self.user = User(username = 'user1')
+        self.resource = Resource(resoucetype = 'Food', description = 'A traditional chinese food')
+
+    def test_string(self):
+        self.assertEqual(str(self.resource), 'Dumplings')
