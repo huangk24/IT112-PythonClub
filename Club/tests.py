3902d50ba111250386a68d159f05d6374526f744
huangk24 <huangk24@uw.edu>
2022-03-21 15:32:54 -0700

    authentication test

--- Club/tests.py
+++ Club/tests.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from .models import Meeting, MeetingMinutes, Resource, Event
 import datetime
 from .forms import MeetingForm
+from django.urls import reverse_lazy, reverse
 
 # Create your tests here.
 class MeetingTest(TestCase):
@@ -48,11 +49,13 @@ class newMeetingForm(TestCase):
         form = MeetingForm(data)
         self.assertTrue(form.is_valid)
 
-    def test_Meetingform_Invalid(self):
-        data={
-             'meetingtitle': 'Pi Day', 
-             'meetingdate': '2022-03-14', 
-             'agenda': 'Celebrate Pi Day!'
-        }
-        form = MeetingForm(data)
-        self.assertFalse(form.is_valid)
+class New_Meeting_Authetication_Test(TestCase):
+    def setUp(self):
+        self.test_user=User.objecrs.create_user(username='testuser1', password='P@ssw0rd1')
+        self.type=Type.objects.create(typename='Spring break')
+        self.event=event.objects.create(eventtitle = 'Spring break', location = 'Seattle', 
+                           date = datetime.date(2022, 3, 15), description = 'Spring break')
+
+    def test_redirect_if_not_logged_in(self):
+        response.self.client.get(reverse('newproduct'))
+        self.assertRediects(response, '/accounts/login/?next=/Club/newmeeting/')
